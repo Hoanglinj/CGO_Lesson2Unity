fd5fecc544f588265c5371c8387ea1597999650a
Hoanglinj <86936803+Hoanglinj@users.noreply.github.com>
2023-08-12 17:20:21 +0700

    update_lesson03

---
+++ Lesson03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum TargetEnum
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    }
+
+    public float speed;
+
+    public Transform topLeftTarget;
+    public Transform topRightTarget;
+    public Transform bottomLeftTarget;
+    public Transform bottomRightTarget;
+
+    private Transform currentTarget;
+    private TargetEnum nextTarget = TargetEnum.TopLeft;
+    
+    
+    void Start()
+    {
+        currentTarget = topLeftTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = currentTarget.position;
+        Vector3 moveDirection = targetPosition - transform.position;
+
+        float distance = moveDirection.magnitude;
+
+        if (distance > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+        }
+
+        else
+        {
+            SetNextTarget(nextTarget);
+        }
+
+        Vector3 direction = currentTarget.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = targetRotation;
+    }
+
+    private void SetNextTarget(TargetEnum target)
+    {
+        switch (target)
+        {
+            case TargetEnum.TopLeft:
+                currentTarget = topLeftTarget;
+                nextTarget = TargetEnum.TopRight;
+                break;
+            case TargetEnum.TopRight:
+                currentTarget = topRightTarget;
+                nextTarget = TargetEnum.BottomLeft;
+                break;
+            case TargetEnum.BottomLeft:
+                currentTarget = bottomLeftTarget;
+                nextTarget = TargetEnum.BottomRight;
+                break;
+            case TargetEnum.BottomRight:
+                currentTarget = bottomRightTarget;
+                nextTarget = TargetEnum.TopLeft;
+                break;
+        }
+
+    }
+}
