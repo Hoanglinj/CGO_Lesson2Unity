9acbee83ed7d7fb3a84b787655a3f5f068a5142d
Hoanglinj <86936803+Hoanglinj@users.noreply.github.com>
2023-08-28 18:05:37 +0700

    update

---
+++ 2D_Graphic/Assets/Scripts/EnemiesScripts.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesScripts : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int tocDo = 4;
+    public float traiPhai;
+    public bool isFacingRight = true;
+
+    public Animator anim;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        traiPhai = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(traiPhai * tocDo, rb.velocity.y);
+
+        if (isFacingRight == true && traiPhai == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            isFacingRight = false;
+        }
+
+        if (isFacingRight == false && traiPhai == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            isFacingRight = true;
+        }
+
+        //Animation
+        anim.SetFloat("dichuyen", Mathf.Abs(traiPhai));
+        
+    }
+}
